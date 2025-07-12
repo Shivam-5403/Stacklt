b08abdaa668a8b8e59743b9973a359df0046cd05
shivam <shivam.bhundiya5403@gmail.com>
2025-07-12 12:04:35 +0530

    change

--- Frontend/src/services/AuthService.js
+++ Frontend/src/services/AuthService.js
@@ -21,7 +21,7 @@ const AUTH_REST_API_BASE_URL = "http://localhost:5000/api/auth"
 
 export const registerAPICall = (registerObj) => axios.post(AUTH_REST_API_BASE_URL + '/register', registerObj);
 
-export const loginAPICall = (usernameOrEmail, password) => axios.post(AUTH_REST_API_BASE_URL + '/login', { usernameOrEmail, password});
+export const loginAPICall = (email, password) => axios.post(AUTH_REST_API_BASE_URL + '/login', { email, password});
 
 export const storeToken = (token) => localStorage.setItem("token", token);
 
