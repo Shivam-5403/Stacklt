3af71d7e5256770b1fc7870fa8e2a29a67477d90
SumitRaval <sumitraval120@gmail.com>
2025-07-12 11:43:11 +0530

    m2

--- Frontend/src/services/AuthService.js
+++ Frontend/src/services/AuthService.js
@@ -17,7 +17,7 @@
 
 import axios from "axios";
 
-const AUTH_REST_API_BASE_URL = "http://localhost:5000/api/auth"//do
+const AUTH_REST_API_BASE_URL = "http://localhost:5000/api/auth"
 
 export const registerAPICall = (registerObj) => axios.post(AUTH_REST_API_BASE_URL + '/register', registerObj);
 
