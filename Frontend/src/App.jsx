a4789a79676455ff81f495427ac6063b4a7d459d
Bhavyajprajapati <157686673+Bhavyajprajapati@users.noreply.github.com>
2025-07-12 11:22:40 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -30,15 +30,6 @@ function App() {
         {/* http://localhost:8080  */}
         <Route path='/login' element={ <LoginComponent/> }></Route>
 
-        {/* http://localhost:8080  */}
-        <Route path='/todos' element={ 
-          <AuthenticateRoute>
-            <ListTodoComponent/>
-          </AuthenticateRoute>
-           }>
-
-
-        </Route>
         <Route path='/' element={ 
             <HomePage/>
            }>
@@ -48,20 +39,6 @@ function App() {
             <AskQuestion/>
            }>
         </Route>
-
-        {/* http://localhost:8080/add-todo */}
-        <Route path='/add-todo' element = {
-          <AuthenticateRoute>
-            <TodoComponent/>
-
-          </AuthenticateRoute>
-             }></Route>
-
-        {/* http://localhost:8080/update-todo */}
-        <Route path='/update-todo/:id' element = { <AuthenticateRoute>
-            <TodoComponent/>
- 
-          </AuthenticateRoute> }></Route>
 
         {/* http://localhost:8080/register */}
         <Route path='/register' element = { <RegisterComponent/> }></Route>
